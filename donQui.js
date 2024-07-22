93d2e4e6a31cffd3b0818ac8ee4543ec9139cd87
Layton <jihwan8807@naver.com>
2024-07-23 02:26:46 +0900

    기본 크롤링 완료

--- donQui.js
+++ donQui.js
@@ -1,19 +1,53 @@
-// puppeteer을 가져온다.
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
 (async () => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // 새로운 페이지를 연다.
-  const page = await browser.newPage();
-  // 페이지의 크기를 설정한다.
-  await page.setViewport({
-    width: 1366,
-    height: 768,
-  });
-  await page.goto('https://mpglobal.donki.com/ec-web/d/h');
+    await page.goto(
+      'https://mpglobal.donki.com/ec-web/d/gd?gId=I2021062302901&toComments=false?lan=ko-kr'
+    );
+
+    // 메타 태그 정보 수집
+    const productName = await page.title();
+    const description = await page.$eval('meta[name="description"]', (ele) =>
+      ele.getAttribute('content')
+    );
+
+    const productPrice = await page.$eval('.jpPrice', (ele) => ele.textContent);
+    const productWonPrice = await page.$eval(
+      '.price',
+      (ele) => ele.textContent
+    );
+
+    // 이미지 추출
+    const imageUrl = await page.$eval('.defaultImg', (ele) =>
+      ele.getAttribute('src')
+    );
+
+    // JSON 파일 저장
+    console.log(
+      description,
+      productName,
+      productPrice,
+      productWonPrice,
+      imageUrl
+    );
+
+    const outputJSON = JSON.stringify({
+      description,
+      productName,
+      productPrice,
+      productWonPrice,
+      imageUrl,
+    });
+
+    fs.writeFileSync('product_data.json', outputJSON);
+
+    await browser.close();
+  } catch (err) {
+    console.log(err);
+  }
 })();
