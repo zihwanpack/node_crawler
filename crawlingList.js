6733aa42e5e40c56186398ce29a9a9f60ce35b1a
Layton <jihwan8807@naver.com>
2024-07-23 23:38:40 +0900

    크롤링 찐막?

--- crawlingList.js
+++ crawlingList.js
@@ -5,7 +5,7 @@ import { scrapeURLs } from './scrapeURLs.js';
   const browser = await puppeteer.launch({
     waitUntil: 'networkidle0',
     timeout: 600000,
-    // headless: false,
+    headless: false,
   });
 
   const page = await browser.newPage();
@@ -23,12 +23,16 @@ import { scrapeURLs } from './scrapeURLs.js';
   page.waitForNavigation();
   for (const div of divArr) {
     await new Promise((resolve) => setTimeout(resolve, 3000));
+
     await div.click();
     const pages = await browser.pages();
     const newPage = pages[pages.length - 1];
+    await newPage.waitForNavigation({ waitUntil: 'networkidle0' });
+
     const url = await newPage.url();
     urlData.push(url);
     console.log(urlData);
+
     await newPage.close();
     await new Promise((resolve) => setTimeout(resolve, 3000));
   }
