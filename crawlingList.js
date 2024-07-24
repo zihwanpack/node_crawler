fc3b0dce34e462c3c896d5c2e4a1a08fdd1605a3
Layton <jihwan8807@naver.com>
2024-07-24 16:39:19 +0900

    최종 수정

--- crawlingList.js
+++ crawlingList.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import { scrapeURLs } from './scrapeURLs.js';
 import { testJsonFile } from './test.js';
+
 (async () => {
   const urlData = [];
   const browser = await puppeteer.launch({
@@ -10,17 +11,15 @@ import { testJsonFile } from './test.js';
   const pageUrl =
     'https://mpglobal.donki.com/ec-web/d/pcd?titleStr=7Iud7ZKI&gpId=g-i_b_food?lan=ko-kr';
   // 원하는 데이터 파일명
-
   const fileName = 'food_data';
-  const page = await browser.newPage();
   const selector = '.goods-item';
 
+  const page = await browser.newPage();
   await page.setViewport({
     width: 1280,
     height: 720,
     deviceScaleFactor: 1,
   });
-
   page.setDefaultNavigationTimeout(0);
 
   try {
@@ -28,29 +27,22 @@ import { testJsonFile } from './test.js';
     // 크롤링전 페이지 조작을 위한 5초
     await new Promise((resolve) => setTimeout(resolve, 5000));
     await page.waitForSelector(selector);
-
     const divArr = await page.$$(selector);
-    const nextPageButton = await page.$('.ant-pagination-next');
-    console.log(nextPageButton);
 
     for (const div of divArr) {
       const newPagePromise = new Promise((resolve) =>
         browser.once('targetcreated', (target) => resolve(target.page()))
       );
-
       await div.click();
       const newPage = await newPagePromise;
-
       await newPage.waitForNavigation();
-
       const url = newPage.url();
       urlData.push(url);
       await newPage.close();
-
       console.log(urlData);
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
-
+    // url에서 정보 긁어오기
     await scrapeURLs(fileName, urlData);
     await testJsonFile(`${fileName}.json`);
   } catch (error) {
